acbf9051b59ca79d92dc94daeba1f83e59c50231
Kyle Reese <reesekyle1998@gmail.com>
2021-02-22 21:48:03 -0500

    Added charge shot phase

---
+++ classes/opponentPhaseChargeShot.lua
@@ -0,0 +1,48 @@
+-- Imma firin ma lasor
+
+local opponentPhaseChargeShot = {}
+opponentPhaseChargeShot.__index = opponentPhaseChargeShot
+
+
+--------------------
+-- MAIN CALLBACKS --
+--------------------
+
+function opponentPhaseChargeShot.new()
+	local self = classes.opponentBase.new(opponentPhaseChargeShot)
+
+	self.CHARGE_TIME = 3
+	self.shot_cooldown = self.CHARGE_TIME
+	self.bulletSpeed = 1000
+
+	return self
+end
+
+function opponentPhaseChargeShot:update(dt)
+	-- Call superclass method
+	classes.opponentBase.update(self, dt)
+
+	if self.stunned then return end
+
+	self.angle = math.atan2(state.player.y - self.y, state.player.x - self.x)
+
+	self.shot_cooldown = self.shot_cooldown - dt
+		if self.shot_cooldown <= 0 then
+			local big_bullet = classes.bullet.new(self.x, self.y, self.angle, false, self.bulletSpeed)
+			big_bullet.radius = 100
+			table.insert(state.bullets, big_bullet)
+			self.shot_cooldown = self.shot_cooldown + self.CHARGE_TIME
+		end
+end
+
+function opponentPhaseChargeShot:draw()
+	-- Optional - draw default opponent
+	classes.opponentBase.draw(self)
+end
+
+function opponentPhaseChargeShot:onDestroy()
+	-- Call default superclass method
+	classes.opponentBase.onDestroy(self)
+end
+
+classes.opponentPhaseChargeShot = opponentPhaseChargeShot
--- classes/opponentSpawner.lua
+++ classes/opponentSpawner.lua
@@ -13,6 +13,7 @@ function opponentSpawner.new()
 		classes.opponentPhaseBouncing,
 		classes.opponentPhaseNoBehavior,
 		classes.opponentPhaseCharge,
+		classes.opponentPhaseChargeShot,
 		classes.opponentPhaseChase,
 		classes.opponentPhaseSpin,
 		classes.opponentPhaseOrbit,
