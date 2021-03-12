4cd7d817de6011fcb08064d5c3a2b608361864c2
Kyle Reese <reesekyle1998@gmail.com>
2021-03-11 21:04:36 -0500

    Added turn speed caps to chase-esque phases

--- classes/opponentPhaseChase.lua
+++ classes/opponentPhaseChase.lua
@@ -17,6 +17,8 @@ function opponentPhaseChase.new()
 	local self = classes.opponentBase.new(opponentPhaseChase)
 
 	self.CHASE_SPEED = 400
+	self.MAX_TURN_SPEED = 2
+	self.angle = math.atan2(state.player.y - self.y, state.player.x - self.x)
 
 	return self
 end
@@ -29,9 +31,32 @@ function opponentPhaseChase:update(dt)
 
 	if not state.player then return end
 
-	self.angle = math.atan2(state.player.y - self.y, state.player.x - self.x)
+	local newAngle = math.atan2(state.player.y - self.y, state.player.x - self.x)
+	local diff = normalizeAngle(newAngle - self.angle)
+	if diff > self.MAX_TURN_SPEED * dt then
+		self.angle = self.angle + self.MAX_TURN_SPEED * dt
+	elseif diff < -self.MAX_TURN_SPEED * dt then
+		self.angle = self.angle - self.MAX_TURN_SPEED * dt
+	else
+		self.angle = newAngle
+	end
+	self.angle = normalizeAngle(self.angle)
 	self.x = self.x + math.cos(self.angle) * self.CHASE_SPEED * dt
 	self.y = self.y + math.sin(self.angle) * self.CHASE_SPEED * dt
+
+	-- Collide with walls
+	if self.x < self.radius then
+		self.x = self.radius
+	end
+	if self.x > ARENA_WIDTH - self.radius then
+		self.x = ARENA_WIDTH - self.radius
+	end
+	if self.y < self.radius then
+		self.y = self.radius
+	end
+	if self.y > ARENA_HEIGHT - self.radius then
+		self.y = ARENA_HEIGHT - self.radius
+	end
 end
 
 function opponentPhaseChase:draw()
