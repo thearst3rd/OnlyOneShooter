1ece1d241307dbbad66d9e24f624c185f2a58264
Terry Hearst <thearst3rd@gmail.com>
2021-04-01 21:43:56 -0400

    Improved charge shot phase

--- classes/opponentPhaseChargeShot.lua
+++ classes/opponentPhaseChargeShot.lua
@@ -4,6 +4,11 @@ local opponentPhaseChargeShot = {}
 opponentPhaseChargeShot.__index = opponentPhaseChargeShot
 
 
+local AIM_TIME = 2
+local CHARGE_TIME = 0.6
+local TOTAL_TIME = AIM_TIME + CHARGE_TIME
+
+
 --------------------
 -- MAIN CALLBACKS --
 --------------------
@@ -11,8 +16,7 @@ opponentPhaseChargeShot.__index = opponentPhaseChargeShot
 function opponentPhaseChargeShot.new()
 	local self = classes.opponentBase.new(opponentPhaseChargeShot)
 
-	self.CHARGE_TIME = 3
-	self.shotCooldown = self.CHARGE_TIME
+	self.timer = AIM_TIME / 2
 	self.bulletSpeed = 1000
 	self.angle = math.atan2(state.player.y - self.y, state.player.x - self.x)
 
@@ -27,19 +31,27 @@ function opponentPhaseChargeShot:update(dt)
 
 	if not state.player then return end
 
-	self.angle = math.atan2(state.player.y - self.y, state.player.x - self.x)
-
-	self.shotCooldown = self.shotCooldown - dt
-	if self.shotCooldown <= 0 then
+	self.timer = self.timer + dt
+	if self.timer <= AIM_TIME then
+		self.angle = math.atan2(state.player.y - self.y, state.player.x - self.x)
+	elseif self.timer > TOTAL_TIME then
 		local bigBullet = classes.bullet.new(self.x, self.y, self.angle, false, self.bulletSpeed)
 		bigBullet.radius = 100
 		table.insert(state.bullets, bigBullet)
-		self.shotCooldown = self.shotCooldown + self.CHARGE_TIME
+		self.timer = self.timer - TOTAL_TIME
 	end
 end
 
 function opponentPhaseChargeShot:draw()
 	-- Optional - draw default opponent
+	if self.timer > AIM_TIME then
+		local chargeLerp = (self.timer - AIM_TIME) / CHARGE_TIME
+		local prevLineWidth = love.graphics.getLineWidth()
+		love.graphics.setLineWidth(4)
+		love.graphics.setColor(0, 0, 0, math.min(chargeLerp * 2, 1))
+		love.graphics.circle("line", self.x, self.y, (1 - chargeLerp) * 300)
+		love.graphics.setLineWidth(prevLineWidth)
+	end
 	classes.opponentBase.draw(self)
 end
 
