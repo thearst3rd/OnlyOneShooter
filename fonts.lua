6b31049a45212463a3e842be5c37189902b0278b
Terry Hearst <thearst3rd@gmail.com>
2021-05-13 22:10:29 -0400

    Implemented credits page

--- fonts.lua
+++ fonts.lua
@@ -8,6 +8,7 @@ function loadFonts()
 	-- Create some fonts
 	fonts.small = love.graphics.newFont(14, "normal", dpiScale)
 	fonts.medium = love.graphics.newFont(22, "normal", dpiScale)
+	fonts.mediumlarge = love.graphics.newFont(28, "normal", dpiScale)
 	fonts.large = love.graphics.newFont(40, "normal", dpiScale)
 	fonts.title = love.graphics.newFont(96, "normal", dpiScale)
 end
